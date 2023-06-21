56c69473167fd8b5de31ce077e0bb1b1c46c8a00
DevRogerio <130984908+DevRogerio@users.noreply.github.com>
2023-06-21 10:17:36 -0300

    Update ed1_ap2.dart

--- ed1_ap2.dart
+++ ed1_ap2.dart
@@ -1,1 +1,12 @@
 # academy_pratica_e1
+void main() {
+  const v1 = 9.0;
+  const v2 = 6.7;
+  const v3 = 2.0;
+  const v4 = 4.7;
+  const media = (v1 + v2 + v3 + v4) / 4;
+
+  print(
+    'A media entre [ ${v1.toStringAsFixed(1)} , ${v2.toStringAsFixed(1)} , ${v3.toStringAsFixed(1)} , ${v4.toStringAsFixed(1)} ] = ${media.toStringAsFixed(1)}',
+  );
+}
